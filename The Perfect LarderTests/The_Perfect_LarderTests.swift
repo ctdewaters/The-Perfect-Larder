ff0427d5d5b95cf675c1b83ee6dd4ae2d930b70a
Collin DeWaters <ctdewaters@icloud.com>
2019-05-16 08:17:53 -0400

    Initial Commit

---
+++ The Perfect LarderTests/The_Perfect_LarderTests.swift
@@ -0,0 +1,34 @@
+//
+//  The_Perfect_LarderTests.swift
+//  The Perfect LarderTests
+//
+//  Created by Collin DeWaters on 5/16/19.
+//  Copyright © 2019 Collin DeWaters. All rights reserved.
+//
+
+import XCTest
+@testable import The_Perfect_Larder
+
+class The_Perfect_LarderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
